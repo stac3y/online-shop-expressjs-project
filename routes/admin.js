591c2126e57e34ac1ae33623cef8c79c822723ce
stac3y <stacy.starina@gmail.com>
2021-07-15 20:36:21 +0300

    root path

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,13 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDirectory = require('../util/path');
+
 const router = express.Router();
 
 router.get('/add-product', (req, res) => {
     console.log('In  the add-product middleware!');
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    res.sendFile(path.join(rootDirectory, 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res) => {
--- routes/shop.js
+++ routes/shop.js
@@ -2,11 +2,13 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDirectory = require('../util/path');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
     console.log('In the home middleware!');
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+    res.sendFile(path.join(rootDirectory, 'views', 'shop.html'));
 });
 
 module.exports = router;
