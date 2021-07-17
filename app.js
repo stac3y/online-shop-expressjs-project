7104ec31b5c576ef78d815c0db302a09a8c856c7
stac3y <stacy.starina@gmail.com>
2021-07-17 15:46:22 +0300

    Написала все страницы на pug

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', 'error-404.html'));
+    res.status(404).render('error-404', {docTitle: 'Page Not Found'})
 })
 
 app.listen(3000)
