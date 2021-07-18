74850bf98568ac608d502f51417d0282b008d050
stac3y <stacy.starina@gmail.com>
2021-07-18 14:06:36 +0300

    Добавила страницы handlebars

--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,7 @@ const adminData = require('./admin');
 
 router.get('/', (req, res) => {
     const products = adminData.products;
-    res.render('shop', {prods: products, docTitle: 'Shop', path:'/'});
+    res.render('shop', {prods: products, docTitle: 'Shop', path:'/', hasProducts: products.length > 0});
 });
 
 module.exports = router;
