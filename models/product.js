f9907f774caf7f5a254baf43c9f9403937c93b63
stac3y <stacy.starina@gmail.com>
2021-07-22 21:32:29 +0300

    Рефакторинг

--- models/product.js
+++ models/product.js
@@ -3,23 +3,28 @@ const path = require('path');
 
 const rootDirectory = require('../util/path');
 
+const p = path.join(
+    rootDirectory,
+    'data',
+    'products.json'
+);
+
+const getProductsFromFile = cb => {
+    fs.readFile(p, (err, data) => {
+        if (err){
+            cb([]);
+        }
+        else cb(JSON.parse(data));
+    })
+}
+
 module.exports = class Product {
     constructor(title) {
         this.title = title;
-
     }
 
     save() {
-        const p = path.join(
-            rootDirectory,
-            'data',
-            'products.json'
-        );
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if (!err) {
-                products = JSON.parse(fileContent);
-            }
+        getProductsFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), err => {
                 console.log(err);
@@ -28,16 +33,6 @@ module.exports = class Product {
     }
 
     static fetchAll(cb) {
-        const p = path.join(
-            rootDirectory,
-            'data',
-            'products.json'
-        );
-        fs.readFile(p, (err, data) => {
-            if (err){
-                cb([]);
-            }
-            cb(JSON.parse(data));
-        })
+        getProductsFromFile(cb);
     }
 }
