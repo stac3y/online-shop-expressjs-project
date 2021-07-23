b5331c3f3afca012d752a1e27d4848962ef14872
stac3y <stacy.starina@gmail.com>
2021-07-23 21:42:28 +0300

    Добавила новые представления

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product')
 
 exports.getAddProduct = (req, res) => {
-    res.render('add-product', {
+    res.render('admin/add-product', {
         docTitle: 'Add product',
         path: '/admin/add-product',
     });
@@ -15,7 +15,7 @@ exports.postAddProduct = (req, res) => {
 
 exports.getProducts = (req, res) => {
     Product.fetchAll(products => {
-        res.render('shop', {
+        res.render('shop/product-list', {
             prods: products,
             docTitle: 'Shop',
             path: '/'
