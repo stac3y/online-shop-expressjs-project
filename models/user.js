a05ca66851e864ec0f57cf792451ef8793c9de18
stac3y <stacy.starina@gmail.com>
2021-08-22 19:05:46 +0300

    Создала модель пользователя

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize');
+
+const sequelize = require('../util/database');
+
+const User = sequelize.define('user', {
+    id:{
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true
+    },
+    name:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email:{
+        type: Sequelize.STRING
+    }
+})
+
+module.exports = User;
