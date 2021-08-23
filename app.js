0c53bd9ce4385c8b2577bb53d0fafc3810daf01b
stac3y <stacy.starina@gmail.com>
2021-08-23 18:09:09 +0300

    Sequelize: Установила связи для корзины

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -36,8 +38,17 @@ app.use(shopRoutes);
 
 app.use(errorController.getError);
 
+//User => Product (one-to-many)
 Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
 User.hasMany(Product);
+
+// User => Cart (one-to-one)
+User.hasOne(Cart);
+Cart.belongsTo(User);
+
+//Cart => Product (many-to-many)
+Cart.belongsToMany(Product, {through: CartItem});
+Product.belongsToMany(Cart, {through: CartItem});
 
 // sequelize.sync({force: true})
 sequelize.sync()
