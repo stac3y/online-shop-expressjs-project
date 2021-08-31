28e1e4e3282e2ea718c8ec7be0afa455eba01bca
stac3y <stacy.starina@gmail.com>
2021-08-31 20:30:06 +0300

    Mongoose: Удаление товаров из корзины

--- models/user.js
+++ models/user.js
@@ -36,6 +36,15 @@ userSchema.methods.addToCart = function (product) {
     return this.save();
 }
 
+userSchema.methods.deleteCartProduct = function (productId) {
+    const updatedCartItems = this.cart.items.filter(item => {
+        return item.productId.toString() !== productId.toString();
+    });
+
+    this.cart = {items: updatedCartItems};
+    return this.save();
+}
+
 
 module.exports = mongoose.model('User', userSchema);
 
