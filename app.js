9efa244f08ac21053675ac7457c2fe0568c1e04f
stac3y <stacy.starina@gmail.com>
2021-09-04 21:20:24 +0300

    Сессии и куки: Форма авторизации

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const errorController = require('./controllers/error');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+
 const User = require('./models/user');
 
 const app = express();
@@ -32,6 +34,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.getError);
 
