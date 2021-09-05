d73785555c8d82494e3daa7b10b14d2fe325786d
stac3y <stacy.starina@gmail.com>
2021-09-05 19:29:20 +0300

    Сессии и куки: Объявление сессии

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ require('custom-env').env('staging');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
 
 const errorController = require('./controllers/error');
 
@@ -20,6 +21,7 @@ app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret: 'my secret', resave: false, saveUninitialized: false}));
 
 app.use((req, res, next) => {
     User.findById("612e37bfbcff225981d3e913")
