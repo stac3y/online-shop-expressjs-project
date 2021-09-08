c5e823d8abb9ed1aa96101751003d2f446b18c6d
stac3y <stacy.starina@gmail.com>
2021-09-08 20:57:24 +0300

    Добавление аутентификации: Вывод сообщения об ошибке/успехе

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,10 +3,25 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
+    let message = req.flash('error');
+    if (message.length > 0) {
+        message = message[0];
+    } else {
+        message = null;
+    }
+    let successMessage = req.flash('success');
+
+    if (successMessage.length > 0) {
+        console.log(successMessage);
+        successMessage = successMessage[0];
+    } else {
+        successMessage = null;
+    }
     res.render('auth/login', {
         docTitle: 'Login',
         path: '/login',
-        errorMessage: req.flash('error')
+        errorMessage: message,
+        successMessage: successMessage
     })
 }
 
@@ -15,13 +30,13 @@ exports.postLogin = (req, res, next) => {
     const password = req.body.password;
     User.findOne({email: email})
         .then(user => {
-            if (!user){
+            if (!user) {
                 req.flash('error', 'Invalid email or password!');
                 return res.redirect('/login');
             }
             bcrypt.compare(password, user.password)
                 .then(isCompare => {
-                    if (!isCompare){
+                    if (!isCompare) {
                         req.flash('error', 'Invalid email or password!');
                         return res.redirect('/login');
                     }
@@ -32,7 +47,7 @@ exports.postLogin = (req, res, next) => {
                         res.redirect('/');
                     });
                 })
-                .catch(err =>{
+                .catch(err => {
                     console.log(err);
                     res.redirect('/login');
                 });
@@ -43,9 +58,18 @@ exports.postLogin = (req, res, next) => {
 }
 
 exports.getSignup = (req, res, next) => {
+    let errorMessage = req.flash('error');
+    if (errorMessage.length > 0) {
+        errorMessage = errorMessage[0];
+    } else {
+        errorMessage = null;
+    }
+
     res.render('auth/signup', {
         docTitle: 'Signup',
-        path: '/signup'
+        path: '/signup',
+        errorMessage: errorMessage,
+        successMessage: null
     })
 }
 
@@ -58,6 +82,7 @@ exports.postSignup = (req, res, next) => {
         .findOne({email: email})
         .then(userDoc => {
             if (userDoc) {
+                req.flash('error', 'E-mail exists already, please pick a different one!');
                 return res.redirect('/signup');
             }
             return bcrypt.hash(password, 12).then(hashedPassword => {
@@ -69,6 +94,7 @@ exports.postSignup = (req, res, next) => {
                 return user.save();
             })
                 .then(result => {
+                    req.flash('success', 'Sign up successfully!');
                     res.redirect('/login');
                 });
         })
