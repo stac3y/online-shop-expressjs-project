e04d46f1385ffe9a9fe85d49f961efc4282cd651
stac3y <stacy.starina@gmail.com>
2021-09-12 15:32:15 +0300

    Расширенная аутентификация: проверка токена

--- controllers/auth.js
+++ controllers/auth.js
@@ -179,3 +179,23 @@ exports.postReset = (req, res, next) => {
             .catch(err => console.log(err))
     }));
 }
+
+exports.getNewPassword = (req, res, next) => {
+    const token = req.params.token;
+    User.findOne({resetToken: token, resetTokenExpiration: {$gt: Date.now()}})
+        .then(user => {
+            let message = req.flash('error');
+            if (message.length > 0) {
+                message = message[0];
+            } else {
+                message = null;
+            }
+            res.render('auth/new-password', {
+                docTitle: 'New Password',
+                path: '/new-password',
+                errorMessage: message,
+                userId: user._id.toString()
+            })
+        })
+        .catch(err => console.log(err));
+}
