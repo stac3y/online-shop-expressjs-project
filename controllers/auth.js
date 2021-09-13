87f2b2b1c84b107fbf94f5faca5fb8ef26c2d2b6
stac3y <stacy.starina@gmail.com>
2021-09-13 20:03:19 +0300

    Валидация: подтверждение пароля

--- controllers/auth.js
+++ controllers/auth.js
@@ -93,7 +93,6 @@ exports.postSignup = (req, res, next) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()){
-        console.log(errors.array());
         return res.status(422).render('auth/signup', {
             docTitle: 'Signup',
             path: '/signup',
