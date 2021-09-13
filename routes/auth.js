5dd550c63c79da2235c5f9cd9f5d20f48fce33c2
stac3y <stacy.starina@gmail.com>
2021-09-13 19:42:29 +0300

    Валидация: пользовательская  валидация

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {check} = require('express-validator');
+const {check, body} = require('express-validator');
 
 const authController = require('../controllers/auth');
 
@@ -11,7 +11,20 @@ router.post('/login', authController.postLogin);
 
 router.get('/signup', authController.getSignup);
 
-router.post('/signup', check('email').isEmail().withMessage('Please enter a valid e-mail!'), authController.postSignup);
+router.post('/signup',
+    [check('email')
+        .isEmail()
+        .withMessage('Please enter a valid e-mail!')
+        .custom((value, {req}) => {
+            if (value === 'test@test.com') {
+                throw new Error('This email address is forbidden!');
+            }
+            return true;
+        })
+    ],
+    authController.postSignup
+)
+;
 
 router.post('/logout', authController.postLogout);
 
