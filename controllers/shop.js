891a66c3197b00b976fc90ecea724ed77c02a546
stac3y <stacy.starina@gmail.com>
2021-09-20 21:21:19 +0300

    Загрузка файлов: генерация pdf

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+const PDFDocument = require('pdfkit');
+
 const Product = require('../models/product');
 const Order = require('../models/order');
 
@@ -169,6 +171,18 @@ exports.getInvoice = (req, res, next) => {
             }
             const invoiceName = `invoice-${orderId}.pdf`;
             const invoicePath = path.join('data', 'invoices', invoiceName);
+
+            const pdfDoc = new PDFDocument();
+
+            res.setHeader('Content-Type', 'application/pdf');
+            res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"');
+
+            pdfDoc.pipe(fs.createWriteStream(invoicePath));
+            pdfDoc.pipe(res);
+
+            pdfDoc.text('Hello, World!');
+
+            pdfDoc.end();
             // fs.readFile(invoicePath, (err, data) => {
             //     if (err) {
             //         return next(err);
@@ -177,10 +191,8 @@ exports.getInvoice = (req, res, next) => {
             //     res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"');
             //     res.send(data);
             // });
-            const file = fs.createReadStream(invoicePath);
-            res.setHeader('Content-Type', 'application/pdf');
-            res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"');
-            file.pipe(res);
+            // const file = fs.createReadStream(invoicePath);
+            // file.pipe(res);
         })
         .catch(err => next(err));
 }
