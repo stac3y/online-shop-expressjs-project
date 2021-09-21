65965fc0be12b55523510ab9df8756b3869a5c60
stac3y <stacy.starina@gmail.com>
2021-09-21 19:56:21 +0300

    Пагинация: добавила пагинацию на страницу с товарами

--- controllers/shop.js
+++ controllers/shop.js
@@ -6,18 +6,34 @@ const PDFDocument = require('pdfkit');
 const Product = require('../models/product');
 const Order = require('../models/order');
 
-const ITEMS_PER_PAGE = 2;
+const ITEMS_PER_PAGE = 1;
 
 exports.getProducts = (req, res, next) => {
+    const page = +req.query.page || 1;
+    let totalItems;
+
     Product.find()
-        .then(products => {
-            console.log(products)
-            res.render('shop/product-list', {
-                prods: products,
-                docTitle: 'All Products',
-                path: '/products'
-            });
+        .countDocuments()
+        .then(numProducts => {
+            totalItems = numProducts;
+            return Product.find()
+                .skip((page - 1) * ITEMS_PER_PAGE)
+                .limit(ITEMS_PER_PAGE)
         })
+        .then(
+            products => {
+                res.render('shop/product-list', {
+                    prods: products,
+                    docTitle: 'All Products',
+                    path: '/products',
+                    currentPage: page,
+                    hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+                    hasPreviousPage: page > 1,
+                    nextPage: page + 1,
+                    previousPage: page - 1,
+                    lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
+                })
+            })
         .catch(err => {
             const error = new Error(err);
             error.httpStatusCode = 500;
