74337ffc5452711e738d52a76179c7976ed81426
stac3y <stacy.starina@gmail.com>
2021-09-21 19:41:58 +0300

    Пагинация: динамическая генерация страниц

--- controllers/shop.js
+++ controllers/shop.js
@@ -44,16 +44,29 @@ exports.getProduct = (req, res, next) => {
 }
 
 exports.getIndex = (req, res, next) => {
-    const page = req.query.page;
+    const page = +req.query.page || 1;
+    let totalItems;
+
     Product.find()
-        .skip((page - 1) * ITEMS_PER_PAGE)
-        .limit(ITEMS_PER_PAGE)
+        .countDocuments()
+        .then(numProducts => {
+            totalItems = numProducts;
+            return Product.find()
+                .skip((page - 1) * ITEMS_PER_PAGE)
+                .limit(ITEMS_PER_PAGE)
+        })
         .then(
             products => {
                 res.render('shop/index', {
                     prods: products,
                     docTitle: 'Shop',
-                    path: '/'
+                    path: '/',
+                    currentPage: page,
+                    hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+                    hasPreviousPage: page > 1,
+                    nextPage: page + 1,
+                    previousPage: page - 1,
+                    lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
                 })
             }
         )
